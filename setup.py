9acc3b3efc7f11f76bf5b2e90e6b6aa4f3459c74
Shailesh Suvarna <77719370+Shailesh7860@users.noreply.github.com>
2025-12-28 09:52:34 +0000

    Fix: remove unused find_packages import from setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Setup configuration for imgdedup package."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
