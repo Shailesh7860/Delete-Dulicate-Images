f3dc1993d3c79b795af2838787646785d6546406
Shailesh Suvarna <77719370+Shailesh7860@users.noreply.github.com>
2025-12-28 08:53:59 +0000

    Refactor: wrap main(), tidy whitespace/typos, add dataset validation

--- imgdedup.py
+++ imgdedup.py
@@ -14,59 +14,75 @@ def dhash(image, hashSize=8):
     # column pixels
     diff = resized[:, 1:] > resized[:, :-1]
     # convert the difference image to a hash and return it
-    return sum([2 ** i for (i, v) in enumerate(diff. flatten()) if v])
+    return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
     # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
- help="path to input dataset")
-ap.add_argument("-r", "--remove", type=int, default=-1,
-help= "whether or not duplicates should be removed (i.e., dry run)")
-args = vars(ap.parse_args())
-# grab the paths to all images in our input dataset directory and
-# then initialize our hashes dictionary
-print("[INFO computing image hashes...")
-imagePaths = list (paths.list_images(args["dataset"]))
-hashes = {}
-# loop over our image paths
-for imagePath in imagePaths:
-    # load the input image and compute the hash
-    image = cv2. imread(imagePath)
-    h = dhash(image)
-    # grab all image paths with that hash, add the current image
-    # path to it, and store the list back in the hashes dictionary
-    p = hashes .get(h, [])
-    p.append(imagePath)
-    hashes[h] = p
-# loop over the image hashes
-for (h, hashedPaths) in hashes.items ():
-# check to see if there is more than one image with the same hash
- if len(hashedPaths) > 1:
-     # check to see if this is a dry run
-     if args["remove"] <= 0:
-        # initialize a montage to store all images with the same
-        # hash
-        montage = None
-        # loop over all image paths with the same hash
-        for p in hashedPaths:
-            # load the input image and resize it to a fixed width
-            # and heightG
-            image - cv2.imread (p)
-            image - cv2.resize(image, (900, 900))
-            # if our montage is None, initialize it
-            if montage is None:
-                montage = image
-            # otherwise, horizontally stack the images
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset", required=True,
+                    help="path to input dataset")
+    ap.add_argument("-r", "--remove", type=int, default=-1,
+                    help="whether or not duplicates should be removed (i.e., dry run)")
+    args = vars(ap.parse_args())
+
+    # basic validation
+    dataset_path = args["dataset"]
+    if not os.path.isdir(dataset_path):
+        raise SystemExit("[ERROR] dataset path does not exist or is not a directory: {}".format(dataset_path))
+    if args["remove"] < -1:
+        raise SystemExit("[ERROR] --remove must be -1 (default), 0 (dry-run) or >0 to remove")
+
+    # grab the paths to all images in our input dataset directory and
+    # then initialize our hashes dictionary
+    print("[INFO] computing image hashes...")
+    imagePaths = list(paths.list_images(dataset_path))
+    hashes = {}
+
+    # loop over our image paths
+    for imagePath in imagePaths:
+        # load the input image and compute the hash
+        image = cv2.imread(imagePath)
+        h = dhash(image)
+
+        # grab all image paths with that hash, add the current image
+        # path to it, and store the list back in the hashes dictionary
+        p = hashes.get(h, [])
+        p.append(imagePath)
+        hashes[h] = p
+
+    # loop over the image hashes
+    for (h, hashedPaths) in hashes.items():
+        # check to see if there is more than one image with the same hash
+        if len(hashedPaths) > 1:
+            # check to see if this is a dry run
+            if args["remove"] <= 0:
+                # initialize a montage to store all images with the same
+                # hash
+                montage = None
+
+                # loop over all image paths with the same hash
+                for p in hashedPaths:
+                    # load the input image and resize it to a fixed width
+                    # and height
+                    image = cv2.imread(p)
+                    image = cv2.resize(image, (900, 900))
+
+                    # if our montage is None, initialize it
+                    if montage is None:
+                        montage = image
+                    # otherwise, horizontally stack the images
+                    else:
+                        montage = np.hstack([montage, image])
+
+                # show the montage for the hash
+                print("[INFO hash: {}]".format(h))
+                #cv2.imshow("Montage", montage)
+                #cv2.waitKey(0)
             else:
-                montage = np.hstack([montage, image])
-        # show the montage for the hash
-        print ("[INFO hash: ()".format(h))
-        #cv2. imshow("Montage", montage)
-        #cv2.waitKey(0)
-     else:
-        # loop over all inage paths with the sane hash "except
-        # for the first image in the list (since we want to ke
-        # one, and only one, of the duplicate inages)
-        for p in hashedPaths[1:]:
-            os.remove(p)
+                # loop over all image paths with the same hash except
+                # for the first image in the list (since we want to keep
+                # one, and only one, of the duplicate images)
+                for p in hashedPaths[1:]:
+                    os.remove(p)
 if __name__ == "__main__":
-    pass
+    main()
